f81a76bbe7359616e6c58726fc6c19fb8aa9525d
obasemola <obasemolaayodele@gmail.com>
2020-09-07 19:04:16 +0300

    app.post

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { request, response } = require('express');
 const app = express()
+app.use(express.json())
 
 let persons = [
   {
@@ -70,6 +72,26 @@ app.delete('/api/persons/:id', (request, response) => {
   const person = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+     })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000)
+  }
+
+  persons = persons.concat(person)
+  
+  response.json(person)
 })
 
 
