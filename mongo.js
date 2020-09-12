6ed223ac24e2cdab97402825f17b1b14e27b8922
obasemola <obasemolaayodele@gmail.com>
2020-09-12 18:56:40 +0300

    refactored code to remove add data bug

--- mongo.js
+++ mongo.js
@@ -19,18 +19,18 @@ const personSchema = new mongoose.Schema({
 
  if (process.argv.length === 3) {
   Person.find({}).then(result => {
-    result.forEach((note) => {
-      console.log(note)
+    result.forEach((person) => {
+      console.log(person.name, person.number)
     })
     mongoose.connection.close()
   })
+} else if (process.argv.length > 3) {
+  const person = new Person()
+  person.name = process.argv[3]
+  person.number = process.argv[4]
+ 
+  person.save().then(result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
-
- const person = new Person()
- person.name = process.argv[3]
- person.number = process.argv[4]
-
- person.save().then(result => {
-   console.log(`added ${person.name} number ${person.number} to phonebook`)
-   mongoose.connection.close()
- })
