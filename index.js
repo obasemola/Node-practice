b249e13e9dccfab0a8f74297cc8ba57b5ca3a4a2
obasemola <obasemolaayodele@gmail.com>
2020-09-15 22:40:48 +0300

    app launch ready

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
 const Person = require('./models/person')
-const morgan = require('morgan');
-const cors = require('cors');
-const { response, query } = require('express');
-const app = express();
+const morgan = require('morgan')
+const cors = require('cors')
+const { response, query } = require('express')
+const app = express()
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 }
@@ -12,16 +12,17 @@ const errorHandler = (error, request, response, next) => {
 const postErrorHandler = (error, request, response, next) => {
   return response.status(409).json({
     error: error.message
-})
+  })
 }
 
-app.use(express.static('build'));
-app.use(express.json());
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors)
 
 
 app.get('/index.html', (request, response) => {
 
-});
+})
 
 
 app.get('/api/persons', (request, response) => {
@@ -31,7 +32,7 @@ app.get('/api/persons', (request, response) => {
 
   })
 
-});
+})
 
 app.get('/info', (request, response) => {
 
@@ -40,7 +41,7 @@ app.get('/info', (request, response) => {
   response.write(`${date}`)
   response.end()
 
-});
+})
 
 
 
@@ -50,7 +51,7 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
   })
 
-});
+})
 
 
 
@@ -58,42 +59,42 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
   Person.findByIdAndRemove(request.params.id)
     .then(result => {response.status(204).end()
-  })
+    })
     .catch(error => next(error))
 
-});
-app.use(errorHandler);
+})
+app.use(errorHandler)
 
 
 
 
 app.post('/api/persons', (request, response, next) => {
 
-  const body = request.body;
+  const body = request.body
 
   if (!body.name) {
     return response.status(400).json({
       error: 'name missing'
-     })
+    })
   } else if (!body.number) {
-    return response.status(400).json({ 
+    return response.status(400).json({
       error: 'number missing'
-     })
-  };
+    })
+  }
 
   const person = new Person({
     name: body.name,
     number: body.number
-  });
+  })
 
 
   person.save()
     .then(savedPerson => {
       response.json(savedPerson)
-  }).catch(error => next(error))
+    }).catch(error => next(error))
 
-});
-app.use(postErrorHandler);
+})
+app.use(postErrorHandler)
 
 
 
@@ -105,23 +106,23 @@ app.put('/api/persons/:id', (request, response, next) => {
     name: body.name,
     number: body.number,
 
-  };
+  }
 
   console.log(person)
 
-  Person.findOne({name: body.name}).then(result => {
+  Person.findOne({ name: body.name }).then(result => {
     if(result) {
       console.log(body.name)
       Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: query })
         .then(updatedPerson => {
-      response.json(updatedPerson)
-      })
+          response.json(updatedPerson)
+        })
         .catch(error => next(error))
     }
   })
 
-});
-app.use(errorHandler);
+})
+app.use(errorHandler)
 
 
 
--- models/person.js
+++ models/person.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
 
 const url = process.env.MONGODB_URI
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -16,14 +16,14 @@ const personSchema = new mongoose.Schema({
     type: Number,
     minlength: 8
   }
- });
+})
 
- personSchema.set('toJSON', {
-   transform: (document, returnedObject) => {
-     returnedObject.id = returnedObject._id.toString()
-     delete returnedObject._id
-     delete returnedObject.__v
-   }
- })
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
